28e44a1cabbaa4fdb7bf7bfd759ca7a1fa0c05e8
ay5876 <ay5876@wayne.edu>
2026-01-28 12:25:58 -0500

    Update test.py
    Fix syntax error in test.py

--- test/test.py
+++ test/test.py
@@ -6,12 +6,10 @@ from cocotb.triggers import ClockCycles
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
+    clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
     # Reset
-    dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
@@ -19,28 +17,20 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
-
-    # Test case 1: A=0, B=0, Cin=0 -> Sum=0, Cout=0
-    dut.ui_in[0].value = 0
-    dut.ui_in[1].value = 0
-    dut.ui_in[2].value = 0
+    # ---- Test case 1: A=0 B=0 Cin=0
+    dut.ui_in.value = 0b000
     await ClockCycles(dut.clk, 10)
     assert dut.uo_out[0].value == 0
     assert dut.uo_out[1].value == 0
 
-    # Test case 2: A=0, B=0, Cin=1 -> Sum=1, Cout=0
-    dut.ui_in[0].value = 0
-    dut.ui_in[1].value = 0
-    dut.ui_in[2].value = 1
+    # ---- Test case 2: A=0 B=0 Cin=1
+    dut.ui_in.value = 0b001
     await ClockCycles(dut.clk, 10)
     assert dut.uo_out[0].value == 1
     assert dut.uo_out[1].value == 0
 
-    # Test case 3: A=0, B=1, Cin=0 -> Sum=1, Cout=0
-    dut.ui_in[0].value = 0
-    dut.ui_in[1].value = 1
-    dut.ui_in[2].value = 0
+    # ---- Test case 3: A=0 B=1 Cin=0
+    dut.ui_in.value = 0b010
     await ClockCycles(dut.clk, 10)
     assert dut.uo_out[0].value == 1
     assert dut.uo_out[1].value == 0
